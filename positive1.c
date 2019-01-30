cafb6078d9e553d0f0ecc15dec92fa3185b4f8e0
swethasreerajendran <43037532+swethasreerajendran@users.noreply.github.com>
2019-01-30 14:42:25 +0530

    Create positive1.c

---
+++ positive1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a<=0)
+ {
+  if(a==0)
+  printf("zero");
+  else
+  printf("negative");
+ }
+else
+ {
+  printf("positive");
+ }
+getch();
+}
